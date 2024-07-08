fe7d5fdbf1c5a42fb3ffb54759b8f4b9a6a9d7e0
Li Kui <lkui@ucsb.edu>
2024-07-08 11:21:05 -1000

    update to download in ERDDAP

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Sea-Surface-temperature
-This is a R script for two tasks: 1. download the daily sst netcdf file from podaac.jpl.nasa.gov; and 2. extract the sst values for given locations' coordinates. 
+This is a R script for two tasks: 1. download the daily sst csv file from ERDDAP server: https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.html; and 2. concat the sst values for all locations listed in the site table. 
 
-The available sst data for this particular satelite starts 2002-06-01. It is not valid to choose an earlier start date when you download the data. Other satellites data on the podaac server provide the sst coverage earlier than 2002. 
+The available sst data for this particular satelite starts 2002-06-01. It is not valid to choose an earlier start date when you download the data. Other satellites data on the ERDDAP server provide the sst coverage earlier than 2002 but with coarser resolution such as: https://coastwatch.pfeg.noaa.gov/erddap/griddap/ncdcOisst21Agg.html
 
-In this code, the Santa Barbara Channel region in Southern California Bight was selected for downloading the daily sst netcdf files. If you want to download other region, you want to adjust the download lat and lon in line 30. For extracting the temperature reading for site coordinates, SBCLTER three sites and the first 10 dates in 2021 are used as examples in the R script.  
+Make sure you have all the unique site coordiantes listed in the site_tb.csv file in the "data" folder. 
 
-If you run into issue during the download process, there is a chance that the data is missing from the server, such as 2021-02-20 and 2021-02-21. You can go into this link to find whether the data is on the server: https://podaac-opendap.jpl.nasa.gov/opendap/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/
+In this code, the Santa Barbara Channel region in Southern California Bight was selected as an example. If you want to download other sites, make sure you have all the unique site coordiantes listed in the site_tb.csv file in the "data" folder.  
 
-For information about the data and data citation, please refer to the website: https://podaac.jpl.nasa.gov/dataset/MUR-JPL-L4-GLOB-v4.1
+For information about the data and data citation, see the summary page on the website: https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.html
+
+The earlier version of the code was to download data from https://podaac.jpl.nasa.gov/dataset/MUR-JPL-L4-GLOB-v4.1. However, the opendap server starts to require a user name and password (EARTHDATA account) to access the data. So we switch to ERDDAP server, which might be changed when updating the next version of the code. 
--- SST_forSBC.R
+++ SST_forSBC.R
@@ -1,93 +1,92 @@
 rm(list = ls())
 
 #-----------------------------------------------------------------------------
-#extract temperature data from SST raster file and compile into one datasheet
-library(reshape2)
-library(dplyr)
-library(ncdf4)
-library(raster)
-library(weathermetrics)
-#https://podaac.jpl.nasa.gov/dataset/MUR-JPL-L4-GLOB-v4.1
+#extract temperature data satellite and compile into one data table
+library(tidyverse)
 
-#acknowlegement:  Please acknowledge the use of these data with the following statement:  These data were provided by JPL under support by NASA MEaSUREs program.
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-#using the date range below as an example
-begin_d <- as.Date("2021-01-01") # The start date for this satellite is 2002-06-01. The earlier date is not valid. 
-end_d<- as.Date("2021-01-10") # the data updated daily, so the end date can be today's date.
+#we used to download data from https://podaac.jpl.nasa.gov/dataset/MUR-JPL-L4-GLOB-v4.1
+#However, the opendap server right now requires a user name and password (EARTHDATA account) to access the data.
+# csv was download from address below
+#download address:
+#https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.html
 
-range <- as.integer(end_d-begin_d)
+# so this version we shift the download to ERDDAP server, which is open to public.
+#The data is the same, but the link is different.
+
+
+#The data is the MUR SST data from JPL, which is a daily global SST data from 2002 to present.
+#The data is in netcdf format, and we will extract the SST data for the Santa Barbara Channel region.
+
+#read in a site table with lon and lat and siteid columns. 
+#Because the data has spatial resolution of 0.01 degree, we create a 0.01 degree box around the each of the coordinates to extract the data.
+#This will give us a SST data with approximately 1 km radials buffer for each location
+sitetb <- read.csv("data/site_tb.csv") %>%
+  mutate(min_lon = longitude - 0.01,
+         max_lon = longitude + 0.01,
+         min_lat = latitude - 0.01,
+         max_lat = latitude + 0.01)
+
+
+#using the date range below as an example
+# note that ERDDAP sometimes will download one more day of data, end_d+1, to make sure data cover the time spam you ask for. 
+begin_d <- as.Date("2024-01-01") # The start date for this satellite is 2002-06-01. The earlier date is not valid. 
+end_d<- as.Date("2024-01-10") # the data updated daily, so the end date can be today's date.
+
+base_url <- "https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.csv"
 
 #show txt progress bar to monitor the download process
-prog.bar <- txtProgressBar(min=0, max=(range+1), style=3)
+prog.bar <- txtProgressBar(min=0, max=(nrow(sitetb)), style=3)
 
-#a loop to download the daily netcdf files from the server, one loop for one day.
-for (i in 1:(range+1)) {
+#a loop to download the daily csv files from the ERDDAP server, one loop for one siteid.
+for (i in 1:nrow(sitetb)) {
   
-  #extract the year
-  year1 = format(begin_d,format="%Y")
+  #site id
+  site <- sitetb$siteid[i]
   
-  #format the start date so the server can recognize it
-  date1 = format(begin_d,format="%Y%m%d") 
-  
-  #calculate the day of year
-  doy <- strftime(begin_d, format = "%j")
-  
-  #Specify where to download the netcdf sst file, from podaac server within Santa Barbara channel region
-  #if your region is outside of santa barbara channel, you need to change the lat and lon range accordingly in the link here. 
-  input.path <- paste0("https://podaac-opendap.jpl.nasa.gov:443/opendap/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/",year1,"/",doy,"/",date1,"090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc.nc4?time[0:1:0],lat[12100:1:12500],lon[5700:1:6300],analysed_sst[0:1:0][12100:1:12500][5700:1:6300]")
+  # Construct the full URL with the appropriate query parameters
+  full_url <- paste0(base_url, "?analysed_sst",
+                     "[(", begin_d,"T00:00:00Z", "):1:(", end_d,"T23:59:59Z", ")]",
+                     "[(", sitetb$min_lat[i], "):1:(", sitetb$max_lat[i], ")]",
+                     "[(", sitetb$min_lon[i], "):1:(", sitetb$max_lon[i], ")]")
   
   #specify where the file will be downloaded to in your local folder, make sure you have a "data" folder with "temperature" subfolder in your working directory 
-  write.path <- paste0("data/temperature/",date1,".nc")
+  write.path <- paste0("data/temperature/",site,".csv")
   
   #download the file and save to the local folder
-  download.file(input.path,write.path,quiet=T,method="auto",mode="wb")
-  
-  #update the date
-  begin_d <- begin_d+1
+  download.file(full_url,write.path,quiet=T,method="auto",mode="wb")
   
   #show the progress bar
   setTxtProgressBar(prog.bar, i) 
 }
 
 #download is finished. 
-#The next step is to extract the sst from the netcdf files for any given coordinates listed in the site table below.
+#The next step is to concat the sst csv files for all sites.
 ###########################################
 
-#read in a site table with long and lat
-site <- read.csv("data/site_tb.csv")
+# Specify the folder containing the CSV files
+folder_path <- "data/temperature/"
 
-begin_d <- as.Date("2021-01-01") # The start date for this satellite is 2002-06-01. The earlier date is not valide. 
-end_d<- as.Date("2021-01-10")
+# Get a list of all CSV files in the folder
+file_list <- list.files(path = folder_path, pattern = "*.csv", full.names = TRUE)
 
-range <- as.integer(end_d-begin_d)
+# Function to read a CSV file and add a column with the file name
+read_and_label_csv <- function(file) {
+  cname <- read.csv(file,nrow=0)
+  df <- read.csv(file, na.strings = "NaN",skip=2)
+  
+  colnames(df) <- colnames(cname)
+  
+  df$siteid <- gsub(".csv", "", basename(file))
+  return(df)
+}
 
-#assign an empty data frame
-data1 <- data.frame()
+# Read each CSV file and store them in a list
+df_list <- lapply(file_list, read_and_label_csv)
 
-for (i in 1:(range+1)) {
-  
-  year1 = format(begin_d,format="%Y")
-  
-  date1 = format(begin_d,format="%Y%m%d") 
-  
-  ncin <- brick(paste0("data/temperature/",date1,".nc"),varname="analysed_sst")
-  
-  #dim(ncin)
-  #print(ncin)
- 
-  ex <- raster::extract(ncin, site[,c("longitude","latitude")])
-  
-  #convert from Kelvin to C
-  temp_C <- convert_temperature(ex,"k","c",round=2)
-  
-  tray <- bind_cols(year=rep(year1,nrow(site)),date=rep(date1,nrow(site)),site_id=site$siteid,temp_C=temp_C) 
-  
-  data1 <- bind_rows(data1,tray)
-  
-  begin_d <- begin_d+1
-  
-}    
+# Concatenate all data frames into one
+final_df <- bind_rows(df_list)
 
 #Export
-write.csv(data1,"data/Sites_temperature_daily.csv",row.names = F)
+write.csv(final_df,"data/Sites_temperature_daily.csv",row.names = F,na="NaN")
